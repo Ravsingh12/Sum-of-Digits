ddf9114f40175d0ffb439d2da9585f1d815de876
admin <admin@CHETUIWK0102>
2022-04-22 15:43:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApplication
+{
+    public class Sumofdigits
+    {
+        public static void Main(string[] args)
+        {
+            int i, sum = 0, j;
+            Console.WriteLine("Enter the number");
+            i = int.Parse(Console.ReadLine());
+            while(i>0)
+            {
+                j = i % 10;
+                sum = sum + j;
+                i = i / 10;
+            }
+            Console.WriteLine("Sum is =" + sum);
+        }
+    }
+}
